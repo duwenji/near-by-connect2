1a783c7fd80a52adaeef0fe3a7cbdc2226da6daf
tofumiyoshi <tofumiyoshi@gmail.com>
2024-12-21 23:15:37 +0900

    Initilaize

---
+++ frontend/src/App.tsx
@@ -0,0 +1,105 @@
+import { Amplify, ResourcesConfig } from "aws-amplify";
+import config from "./config";
+import "@aws-amplify/ui-react/styles.css";
+import { Authenticator } from "@aws-amplify/ui-react";
+import Echo from "./components/echo";
+import { AppBar, Avatar, Button, Container, Select, SelectChangeEvent, Toolbar, Typography, MenuItem } from "@mui/material";
+import { createContext, useEffect, useState } from "react";
+import EchoEvents from "./components/echo-events";
+
+function App() {
+  const [mode, setMode] = useState<"events" | "apigw">("apigw");
+
+  const CurrentLocationContext = createContext<GeolocationCoordinates | undefined>(undefined);
+  const [currentLocation, setCurrentLocation] = useState<GeolocationCoordinates>();
+  
+  /// 位置情報の監視を開始する
+  function registerGeolocationWatch() {    
+    console.log("registerGeolocationWatch");
+  
+    let watchId: number | undefined;
+    if (navigator.geolocation) {
+      watchId = navigator.geolocation.watchPosition((position) => {
+        console.log("watchPosition. New Position: ", position);
+  
+        setCurrentLocation(position.coords);
+      },
+      error => { () => console.error(error); },
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      });
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+    return watchId;
+  }
+
+  const handleModeChange = (e: SelectChangeEvent<"events" | "apigw">) => {
+    setMode(e.target.value as any);
+  };
+
+  const amplifyConfig: ResourcesConfig = {
+    Auth: {
+      Cognito: {
+        userPoolId: config.userPoolId,
+        userPoolClientId: config.userPoolClientId,
+      },
+    },
+    API: {
+      Events: {
+        endpoint: config.eventsEndpoint,
+        defaultAuthMode: "userPool",
+      },
+    },
+  };
+  Amplify.configure(amplifyConfig);
+
+  useEffect(() => { 
+    const watchId = registerGeolocationWatch();
+    return () => {
+      if (watchId) {
+        navigator.geolocation.clearWatch(watchId);
+      }
+    };
+  }, []);
+
+  return (
+    <>
+      <CurrentLocationContext.Provider value={currentLocation}>
+        <Authenticator signUpAttributes={["email"]} loginMechanisms={["email"]}>
+          {({ signOut, user }) => {
+            return (
+              <>
+                <AppBar position="static">
+                  <Toolbar>
+                    <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+                      Real-time events with Cognito authentication
+                    </Typography>
+                    <Avatar />
+                    <Typography sx={{ paddingX: 2 }}>{user == null ? "" : user.username}</Typography>
+                    <Button color="inherit" onClick={signOut}>
+                      Sign out
+                    </Button>
+                  </Toolbar>
+                </AppBar>
+                <main>
+                  <Container maxWidth="lg" sx={{ m: 2 }}>
+                    <Select value={mode} onChange={handleModeChange}>
+                      <MenuItem value={"events"}>AppSync Events</MenuItem>
+                      <MenuItem value={"apigw"}>API Gateway WebSocket</MenuItem>
+                    </Select>
+                    {mode === "events" ? <EchoEvents /> : <Echo />}
+                  </Container>
+                </main>
+              </>
+            );
+          }}
+        </Authenticator>
+      </CurrentLocationContext.Provider>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/echo.tsx
@@ -0,0 +1,117 @@
+import { FC, useEffect, useReducer, useState } from "react";
+import { fetchAuthSession } from "aws-amplify/auth";
+import { Typography, Button, TextField, Stack } from "@mui/material";
+import { SubmitHandler, useForm } from "react-hook-form";
+import config from "../config";
+
+type EchoInput = {
+  message: string;
+};
+
+const Echo: FC = () => {
+  const { register, handleSubmit, reset } = useForm<EchoInput>();
+  const [status, setStatus] = useState("initializing");
+  const [messages, setMessages] = useState<string[]>([]);
+  const [client, setClient] = useState<WebSocket>();
+  const [closed, forceClose] = useReducer(() => true, false);
+
+  const initializeClient = async () => {
+    const currentSession = await fetchAuthSession();
+    const idToken = currentSession.tokens?.idToken;
+
+    const client = new WebSocket(`${config.apiEndpoint}?idToken=${idToken}`);
+
+    client.onopen = () => {
+      setStatus("connected");
+    };
+
+    client.onerror = (e: any) => {
+      setStatus("error (reconnecting...)");
+      console.error(e);
+
+      setTimeout(async () => {
+        await initializeClient();
+      });
+    };
+
+    client.onclose = () => {
+      if (!closed) {
+        setStatus("closed (reconnecting...)");
+
+        setTimeout(async () => {
+          await initializeClient();
+        });
+      } else {
+        setStatus("closed");
+      }
+    };
+
+    client.onmessage = async (message: any) => {
+      const messageStr = JSON.parse(message.data);
+      setMessages((prev) => [...prev, messageStr.message]);
+    };
+
+    setClient(client);
+  };
+
+  const sendMessage: SubmitHandler<EchoInput> = async (input: EchoInput) => {
+    if (client != null) {
+      client.send(input.message);
+      reset({ message: "" });
+    }
+  };
+
+  const handleUserKeyDown = (e: any) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      handleSubmit(sendMessage)(); // this won't be triggered
+    }
+  };
+
+  useEffect(() => {
+    initializeClient();
+    return () => {
+      if (client != null) {
+        forceClose();
+        client.close();
+      }
+    };
+  }, []);
+
+  return (
+    <Stack justifyContent="center" alignItems="center" sx={{ m: 2 }}>
+      <Typography variant="h4" gutterBottom>
+        WebSocket echo demo
+      </Typography>
+
+      <Typography variant="subtitle1" sx={{ color: "#808080" }} gutterBottom>
+        status: {status}
+      </Typography>
+
+      <Stack direction="row" spacing={2} sx={{ m: 5 }}>
+        <TextField
+          id="message"
+          label="Message"
+          size="small"
+          required
+          {...register("message")}
+          autoComplete="off"
+          onKeyDown={handleUserKeyDown}
+          sx={{ width: 400 }}
+        />
+        <Button variant="contained" color="primary" onClick={handleSubmit(sendMessage)}>
+          Send
+        </Button>
+      </Stack>
+
+      <Typography variant="subtitle1" gutterBottom>
+        Messages returned from WebSocket server
+      </Typography>
+
+      {messages.map((msg) => (
+        <Typography sx={{ color: "#808080" }}> {msg}</Typography>
+      ))}
+    </Stack>
+  );
+};
+
+export default Echo;
