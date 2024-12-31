3b11e4b33e4dd6b18951ecb2a6fb2942049ef0bd
tofumiyoshi <tofumiyoshi@gmail.com>
2025-01-01 07:58:56 +0900

    Add currentLocation reducer to manage location updates in Redux store

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,15 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit'
 // ...
 
+const initialCurrentLocation = { lat: 0, lng: 0 };
+
 const store = configureStore({
   reducer: {
     // posts: postsReducer,
     // comments: commentsReducer,
     // users: usersReducer,
+
+    currentLocation: (state = initialCurrentLocation , action) => { 
+      switch (action.type) {
+        case 'UPDATE_LOCATION':
+          return action.payload;
+        default:
+          return state;
+      }
+    },
   },
 })
 
 export type AppStore = typeof store
+
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
